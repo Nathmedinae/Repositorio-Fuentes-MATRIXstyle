51c0352d8b32782d6bfd956123394971a765572a
LuzPulido <lepulidop@gmail.com>
2021-10-10 12:36:15 -0500

    Edici{on Tabla Productos Activos

--- src/components/ModuleProductComp/ModifyProducts.js
+++ src/components/ModuleProductComp/ModifyProducts.js
@@ -5,10 +5,30 @@ function ModifyProducts () {
     return (
         <main>
             <section>
-                <figure>
+                
                     <h2 className="subtitle_page">Modificar Productos</h2>
-                                    
-                </figure>
+
+                <div className="grid">
+
+                <section  class="contenedorSeccion">
+                <span>Búsqueda</span>
+                <form action="">
+                    <label for="codigo">
+                        <span>Código de Producto:</span>
+                        <input type="number" id ="codigo" placeholder="Escribe el Código" autocomplete="off" required/>
+                    </label>
+                    <label for="nombre">
+                        <span>Nombre de Producto:</span>
+                        <input type="text" id ="nombre" placeholder="Escribe el Nombre del Producto" autocomplete="off" required/>
+                    </label>
+                        
+                    <input class="submitButton" type="submit" value="Buscar Producto"/> 
+                </form>    
+                </section>  
+                </div>
+          
+                                          
+                
             </section>
         </main>
     )
