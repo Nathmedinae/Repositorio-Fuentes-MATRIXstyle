75087cd76edb3c3920cf9cea56f970fea425b332
LuzPulido <lepulidop@gmail.com>
2021-10-12 22:59:06 -0500

    Se incluye botón con useEffect en Mostrar/Crear Producto

--- src/components/ModuleProductComp/ActiveProducts.js
+++ src/components/ModuleProductComp/ActiveProducts.js
@@ -1,50 +1,48 @@
-import React from "react";
+import React, {useEffect, useState} from 'react'
 
 
-function ActiveProducts () {
+const ActiveProducts = () => {
+    const [MostrarTabla, setMostrarTabla] = useState(false);
+    const [TextoBoton, setTextoBoton] = useState("Mostrar Productos");
+
+    useEffect(()=>{
+        if (MostrarTabla) {
+            setTextoBoton("Crear Nuevo Producto")
+        }
+        else{
+            setTextoBoton("Mostrar Productos")
+        }
+           
+    },  [MostrarTabla]);
     return (
         <main>
             <section>
+                    
+                <h2 class="subtitle_page">Lista Productos Activos</h2>         
+                     
+                <button onClick={()=>setMostrarTabla(!MostrarTabla)} className='text-white bg-green-500 p-5'>{TextoBoton}</button>
+                {MostrarTabla? <TablaProductos />:<FormularioCreacion />} 
                 
-            <figure>
-                <h2 class="subtitle_page">Lista Productos Activos</h2>
-               
-                <table>
-                    <caption>Activos</caption>
-                    <tr>
-                       <th>Estado</th>
-                       <th>Imagen</th>
-                       <th>Producto</th>
-                       <th>SKU</th>
-                       <th>Inventario</th>
-                    </tr>
-                    <tr>
-                        <td>Activo</td>
-                        <td>Imagen</td>
-                        <td>Camiseta Blanca</td>
-                        <td>10000</td>
-                        <td>100</td>
-                     </tr>
-                     <tr>
-                        <td>Activo</td>
-                        <td>Imagen</td>
-                        <td>Camiseta Negra</td>
-                        <td>20000</td>
-                        <td>0</td>
-                     </tr>
-                     <tr>
-                        <td>Activo</td>
-                        <td>Imagen</td>
-                        <td>Camiseta Gris</td>
-                        <td>10000</td>
-                        <td>100</td>
-                     </tr>
-                </table>
-
-            </figure>
-        </section>
+                </section>
         </main>                     
     )
-};
+    
+}
+
+const FormularioCreacion = () => {
+    return(
+        <div>
+            Esto es otro div pero aquí irà el formulario para crear productos
+        </div>
+    )
+} 
+const TablaProductos = () =>{   
+    return(
+        <div>
+        Esto es un div pero aquí debería ir una tabla
+
+        </div>
+    )
+}
 
 export {ActiveProducts};
